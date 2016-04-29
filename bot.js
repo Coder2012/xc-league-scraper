f01781c0e10067246b5513d7aa638e1ffe14ad52
Neil Brown <nbrown@amaze.com>
2016-04-29 08:54:40 +0100

    scraper now adds url specific identifier to each entry

--- bot.js
+++ bot.js
@@ -1,52 +1,51 @@
 var Model = require('./model');
 var Scraper = require('./scraper');
-var Pages = [];
+var pages = [];
 var domain = 'http://www.xcleague.com';
 var flightUrls = [];
 
 function generateUrls(limit) {
-  // test url for errors
-  //http://www.xcleague.com/xc/flights/20142100.html?vx=0111
-  var url = domain + '/xc/leagues/all-1.html';
-  var urls= [url];
+    // test url for errors
+    //http://www.xcleague.com/xc/flights/20142100.html?vx=0111
+    var url = domain + '/xc/leagues/all-1.html';
+    var urls = [url];
 
-  return urls;
+    return urls;
 }
 
-// store all urls in a global variable  
-Pages = generateUrls();
+pages = generateUrls();
 
 function scrapePilots() {
-  // if the Pages array is empty, we are Done!!
-  if (!Pages.length) {
-    return console.log('Done!!!!');
-  }
-  var url = Pages.pop();
-  var scraper = new Scraper(url);
-  var model;
-  console.log('Requests Left: ' + Pages.length);
-  // if the error occurs we still want to create our
-  // next request
-  scraper.on('error', function (error) {
-    console.log('bot error received from Scraper: ', error);
-    scraper.resume();
-  });
-  // if the request completed successfully
-  // we want to store the results in our database
-  scraper.on('complete', function (models) {
-    
-    for (var i = 0; i < models.length; i++) {
-      model = new Model(models[i]);
-      // console.log(models[i]);
-      model.save(function(err) {
-        if (err) {
-          console.log('Database err saving: ' + url);
-        }
-      });
-    };
-    console.log("all models saved to MongoDB");
 
-  });
+    if (!pages.length) {
+        console.log("All pages now scraped");
+        return;
+    }
+
+    var url = pages.pop();
+    var scraper = new Scraper(url);
+    var model;
+
+    console.log('Requests Left: ' + pages.length);
+
+    scraper.on('error', function(error) {
+        console.log('bot error received from Scraper: ', error);
+        scraper.resume();
+    });
+
+    scraper.on('complete', function(models) {
+
+        for (var i = 0; i < models.length; i++) {
+            model = new Model(models[i]);
+            model.save(function(err) {
+                if (err) {
+                    console.log('Database error saving: ' + url);
+                }
+            });
+        };
+        console.log("All models saved to MongoDB");
+
+    });
 }
 
 scrapePilots();
--- scraper.js
+++ scraper.js
@@ -4,49 +4,43 @@ var moment = require('moment');
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 var STATUS_CODES = http.STATUS_CODES;
-/*
- * Scraper Constructor
-**/
-function Scraper (url) {
+var domain = 'http://www.xcleague.com';
+var regex = /\d+/g;
+var id;
+
+function Scraper(url) {
     this.index = 0;
     this.url = url;
     this.urls = [];
     this.init();
 }
-/*
- * Make it an EventEmitter
-**/
+
 util.inherits(Scraper, EventEmitter);
 
-/*
- * Initialize scraping
-**/
-Scraper.prototype.init = function () {
+Scraper.prototype.init = function() {
     var self = this;
     self.models = [];
 
-    self.on('loadedPilotPage', function (html) {
+    self.on('loadedPilotPage', function(html) {
         self.urls = self.parseLeaguePage(html);
-        console.log("urls[index]: ", self.urls[this.index]);
-        self.url = 'http://www.xcleague.com' + self.urls[this.index];
+        self.url = domain + self.urls[this.index];
         self.loadPage('loadedFlightPage');
     });
 
-    self.on('loadedFlightPage', function (html) {
+    self.on('loadedFlightPage', function(html) {
         var nextUrl = self.parseFlightPage(html);
-        if(nextUrl !== undefined && nextUrl !== 'undefined'){
-          console.log("loadedFlightPage: ", nextUrl);
-          self.url = 'http://www.xcleague.com' + nextUrl;
-          self.loadPage('loadedFlightPage');
-        }else{
-          if(this.index < self.urls.length - 1){
-            this.index ++;
-            self.url = 'http://www.xcleague.com' + self.urls[this.index];
-            console.log("index: ", self.url);
+        if (nextUrl !== undefined && nextUrl !== 'undefined') {
+            console.log("loadedFlightPage: ", nextUrl);
+            self.url = domain + nextUrl;
             self.loadPage('loadedFlightPage');
-          }else{
-            self.emit('complete', self.models);
-          }
+        } else {
+            if (this.index < self.urls.length - 1) {
+                this.index++;
+                self.url = domain + self.urls[this.index];
+                self.loadPage('loadedFlightPage');
+            } else {
+                self.emit('complete', self.models);
+            }
         }
     });
 
@@ -54,181 +48,188 @@ Scraper.prototype.init = function () {
 };
 
 Scraper.prototype.resume = function() {
-  this.emit('loadedFlightPage');
+    this.emit('loadedFlightPage');
 }
 
-Scraper.prototype.loadPage = function (eventName) {
-  var self = this;
-  console.log('\n\nLoading: ', self.url);
-  http.get(self.url, function (res) {
-    var body = '';
-    if(res.statusCode !== 200) {
-      return self.emit('error', STATUS_CODES[res.statusCode]);
-    }
-    res.on('data', function (chunk) {
-      body += chunk;
-    });
-    res.on('end', function () {
-      self.emit(eventName, body);
-    });
-  })
-  .on('error', function (err) {
-    self.emit('error', err);
-  });      
-};
-
-/*
- * Parse html and return an object
-**/
-Scraper.prototype.parseLeaguePage = function (html) {
-  var $ = cheerio.load(html);
-  var table = $('#leagueTable');
-
-  // for testing use one entry
-  // var rows = table.find('tr').eq(399);
-
-  // production use all entries
-  var rows = table.find('tr');
-  var flights = [];
-
-  rows.each(function(index, el){
-  	var row = $(el);
-  	var tds = row.find('td');
-    var flight = tds.eq(6).find('a').eq('1').attr('href');
+Scraper.prototype.loadPage = function(eventName) {
+    var self = this;
+    id = self.url.match(regex)[0];
+    console.log('\n\nLoading: ', self.url);
+    console.log("id: ", id);
     
-    if(flight !== undefined){
-      console.log("flight: ", flight);
-      flights.push(flight);
-    }
-  });
-
-  return flights;
+    http.get(self.url, function(res) {
+        var body = '';
+        if (res.statusCode !== 200) {
+            return self.emit('error', STATUS_CODES[res.statusCode]);
+        }
+        res.on('data', function(chunk) {
+            body += chunk;
+        });
+        res.on('end', function() {
+            self.emit(eventName, body);
+        });
+    })
+    .on('error', function(err) {
+        self.emit('error', err);
+    });
 };
 
-Scraper.prototype.parseFlightPage = function(html){
-  if(html === undefined){
-    console.log("return")
-    return undefined;
-  }
-  console.log("call to parse")
-  var self = this;
-  var $ = cheerio.load(html);
+Scraper.prototype.parseLeaguePage = function(html) {
+    var $ = cheerio.load(html);
+    var table = $('#leagueTable');
 
-  $('#coordinates').remove();
+    // for testing use one entry
+    // var rows = table.find('tr').eq(399);
 
-  var pilot = $('#hpTitle').html().replace(/\n|<span.*<\/span>/gi, "");
-  var title = $('.vfFlightText').text();
+    var rows = table.find('tr');
+    var flights = [];
 
-  var club = '', glider = '', date = '', start = '', finish = '', duration = '', takeoff = '', landing = '', total = '', multiplier = '', score = '';
+    rows.each(function(index, el) {
+        var row = $(el);
+        var tds = row.find('td');
+        var flight = tds.eq(6).find('a').eq('1').attr('href');
 
-  var totalCollected = false;
-
-  $('.viewRow').each(function(index, el){
-    var $el = $(el);
-    var label = $el.find('.viewLabel').text();
-    var text = $el.find('.viewText').text();
-
-    switch(label){
-      case 'Club':
-        club = text;
-      break;
-
-      case 'Glider':
-        glider = text;
-      break;
-
-      case 'Date':
-        date = text;
-      break;
-
-      case 'Start':
-        start = text;
-      break;
-
-      case 'Finish':
-        finish = text;
-      break;
-
-      case 'Duration':
-        duration = text;
-      break;
-
-      case 'Takeoff':
-        takeoff = text;
-      break;
-
-      case 'Landing':
-        landing = text;
-      break;
-
-      case 'Total':
-        if(!totalCollected){
-          totalCollected = true;
-          total = text;
+        if (flight !== undefined) {
+            flights.push(flight);
         }
-      break;
+    });
 
-      case 'Multiplier':
-        multiplier = text;
-      break;
+    return flights;
+};
 
-      case 'Score':
-        score = text;
-      break;
-
-      default:
-        0;
+Scraper.prototype.parseFlightPage = function(html) {
+    if (html === undefined) {
+        return undefined;
     }
-  });
 
-  // get stats from panel
-  var stats = $('#xcTab-stats-content');
-  var maxHeight = stats.find('#xcTab-stats-height-max').text();
-  var lowHeight = stats.find('#xcTab-stats-height-low').text();
-  var takeoffHeight = stats.find('#xcTab-stats-height-ta').text();
-  var maxClimb = stats.find('#xcTab-stats-climb-max').text();
-  var minClimb = stats.find('#xcTab-stats-climb-min').text();
-  var maxSpeed = stats.find('#xcTab-stats-speed-max').text();
-  var avgSpeedCourse = stats.find('#xcTab-stats-speed-avgCourse').text();
-  var avgSpeedTrack = stats.find('#xcTab-stats-speed-avgCourse').text();pilot
+    var self = this;
+    var $ = cheerio.load(html);
 
-  var model = {
-    identifier: pilot + start + finish + score,
-    pilot: pilot,
-    title: title,
-    club: club,
-    glider: glider,
-    date: moment(date, 'DD MMM YYYY'),
-    start: start,
-    startNum: parseFloat(start || 0),
-    finish: finish,
-    finishNum: parseFloat(finish || 0),
-    duration: duration,
-    durationNum: parseFloat(duration || 0),
-    takeoff: takeoff,
-    landing: landing,
-    total: parseFloat(total || 0),
-    multiplier: multiplier,
-    score: parseFloat(score || 0),
-    maxHeight: parseFloat(maxHeight || 0),
-    lowHeight: parseFloat(lowHeight || 0),
-    takeoffHeight: parseFloat(takeoffHeight || 0),
-    maxClimb: parseFloat(maxClimb || 0),
-    minClimb: parseFloat(minClimb || 0),
-    maxSpeed: parseFloat(maxSpeed || 0),
-    avgSpeedCourse: parseFloat(avgSpeedCourse || 0),
-    avgSpeedTrack: parseFloat(avgSpeedTrack || 0)
-  }
+    $('#coordinates').remove();
 
-  self.models.push(model);
+    var pilot = $('#hpTitle').html().replace(/\n|<span.*<\/span>/gi, "");
+    var title = $('.vfFlightText').text();
 
-  var nextUrl = $('.navNext a').attr('href');
-  if(nextUrl !== 'undefined' && nextUrl !== undefined){
-    console.log("nextUrl: ", nextUrl);
-    return nextUrl;
-  }else{
-    console.log("no more urls");
-  }
+    var club = '',
+        glider = '',
+        date = '',
+        start = '',
+        finish = '',
+        duration = '',
+        takeoff = '',
+        landing = '',
+        total = '',
+        multiplier = '',
+        score = '';
+
+    var totalCollected = false;
+
+    $('.viewRow').each(function(index, el) {
+        var $el = $(el);
+        var label = $el.find('.viewLabel').text();
+        var text = $el.find('.viewText').text();
+
+        switch (label) {
+            case 'Club':
+                club = text;
+                break;
+
+            case 'Glider':
+                glider = text;
+                break;
+
+            case 'Date':
+                date = text;
+                break;
+
+            case 'Start':
+                start = text;
+                break;
+
+            case 'Finish':
+                finish = text;
+                break;
+
+            case 'Duration':
+                duration = text;
+                break;
+
+            case 'Takeoff':
+                takeoff = text;
+                break;
+
+            case 'Landing':
+                landing = text;
+                break;
+
+            case 'Total':
+                if (!totalCollected) {
+                    totalCollected = true;
+                    total = text;
+                }
+                break;
+
+            case 'Multiplier':
+                multiplier = text;
+                break;
+
+            case 'Score':
+                score = text;
+                break;
+
+            default:
+                0;
+        }
+    });
+
+    // get stats from panel
+    var stats = $('#xcTab-stats-content');
+    var maxHeight = stats.find('#xcTab-stats-height-max').text();
+    var lowHeight = stats.find('#xcTab-stats-height-low').text();
+    var takeoffHeight = stats.find('#xcTab-stats-height-ta').text();
+    var maxClimb = stats.find('#xcTab-stats-climb-max').text();
+    var minClimb = stats.find('#xcTab-stats-climb-min').text();
+    var maxSpeed = stats.find('#xcTab-stats-speed-max').text();
+    var avgSpeedCourse = stats.find('#xcTab-stats-speed-avgCourse').text();
+    var avgSpeedTrack = stats.find('#xcTab-stats-speed-avgCourse').text();
+
+    var model = {
+        identifier: id,
+        pilot: pilot,
+        title: title,
+        club: club,
+        glider: glider,
+        date: moment(date, 'DD MMM YYYY'),
+        start: start,
+        startNum: parseFloat(start || 0),
+        finish: finish,
+        finishNum: parseFloat(finish || 0),
+        duration: duration,
+        durationNum: parseFloat(duration || 0),
+        takeoff: takeoff,
+        landing: landing,
+        total: parseFloat(total || 0),
+        multiplier: multiplier,
+        score: parseFloat(score || 0),
+        maxHeight: parseFloat(maxHeight || 0),
+        lowHeight: parseFloat(lowHeight || 0),
+        takeoffHeight: parseFloat(takeoffHeight || 0),
+        maxClimb: parseFloat(maxClimb || 0),
+        minClimb: parseFloat(minClimb || 0),
+        maxSpeed: parseFloat(maxSpeed || 0),
+        avgSpeedCourse: parseFloat(avgSpeedCourse || 0),
+        avgSpeedTrack: parseFloat(avgSpeedTrack || 0)
+    }
+
+    self.models.push(model);
+
+    var nextUrl = $('.navNext a').attr('href');
+    if (nextUrl !== 'undefined' && nextUrl !== undefined) {
+        console.log("nextUrl: ", nextUrl);
+        return nextUrl;
+    } else {
+        console.log("no more urls");
+    }
 }
 
 module.exports = Scraper;
